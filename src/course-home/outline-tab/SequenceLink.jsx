f2643c722789d9e48afa2e74f6397b76a69b9996
Dawid Leśniak <dawid.lesniak@emag.lukasiewicz.gov.pl>
2024-11-13 18:53:56 +0100

    Centering icons

--- src/course-home/outline-tab/SequenceLink.jsx
+++ src/course-home/outline-tab/SequenceLink.jsx
@@ -88,7 +88,7 @@ const SequenceLink = ({
   return (
     <li>
       <div className={classNames('', { 'mt-2 pt-2 border-top border-light': !first })}>
-        <div className="row w-100 m-0">
+        <div className="d-flex align-items-center row w-100 m-0">
           <div className="col-auto p-0">
             {complete ? (
               <FontAwesomeIcon
