74e727f86a6f078a2b2c3359d4d076bbdc0564ce
pawel.lizner <pawel.lizner@gmail.com>
2024-11-29 12:52:51 +0100

    CUD-2662 - faBook icon regular smaller

--- src/courseware/course/sequence/sequence-navigation/UnitIcon.jsx
+++ src/courseware/course/sequence/sequence-navigation/UnitIcon.jsx
@@ -10,8 +10,8 @@ export function BookRegularIcon({
 }) {
   return (
     <svg
-      width="19"
-      height="23"
+      width="18"
+      height="20"
       viewBox="0 0 19 23"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
