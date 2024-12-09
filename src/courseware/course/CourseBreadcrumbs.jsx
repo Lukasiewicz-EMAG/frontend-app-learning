97f75771b0f1cc8f7ff0d971b4043b9c2af06c8c
pawel.lizner <pawel.lizner@gmail.com>
2024-12-09 13:37:13 +0100

    CUD-2667 - added title

--- src/courseware/course/CourseBreadcrumbs.jsx
+++ src/courseware/course/CourseBreadcrumbs.jsx
@@ -170,7 +170,9 @@ const CourseBreadcrumbs = ({
   console.log('links:', links);
 
   return (
-    <nav aria-label="breadcrumb" className="d-inline-block col-sm-10 mb-3">
+    <>
+      <h2>{course.title}</h2>
+          <nav aria-label="breadcrumb" className="d-inline-block col-sm-10 mb-3">
       <ol className="list-unstyled d-flex flex-nowrap align-items-center m-0">
         <li className="list-unstyled col-auto m-0 p-0">
           <Link
@@ -200,8 +202,97 @@ const CourseBreadcrumbs = ({
         ))}
       </ol>
     </nav>
+    </>
+
   );
 };
+
+const CourseBreadcrumbs = ({
+  courseId,
+  sectionId,
+  sequenceId,
+  unitId,
+  isStaff,
+}) => {
+  const course = useModel('coursewareMeta', courseId);
+  const courseStatus = useSelector((state) => state.courseware.courseStatus);
+  const sequenceStatus = useSelector((state) => state.courseware.sequenceStatus);
+
+  const allSequencesInSections = Object.fromEntries(
+    useModels('sections', course.sectionIds).map((section) => [
+      section.id,
+      {
+        default: section.id === sectionId,
+        title: section.title,
+        sequences: useModels('sequences', section.sequenceIds),
+      },
+    ]),
+  );
+
+  const links = useMemo(() => {
+    const chapters = [];
+    const sequentials = [];
+    if (courseStatus === 'loaded' && sequenceStatus === 'loaded') {
+      Object.entries(allSequencesInSections).forEach(([id, section]) => {
+        chapters.push({
+          id,
+          label: section.title,
+          default: section.default,
+          sequences: section.sequences,
+        });
+        if (section.default) {
+          section.sequences.forEach((sequence) => {
+            sequentials.push({
+              id: sequence.id,
+              label: sequence.title,
+              default: sequence.id === sequenceId,
+              sequences: [sequence],
+            });
+          });
+        }
+      });
+    }
+    return [chapters, sequentials];
+  }, [courseStatus, sequenceStatus, allSequencesInSections, sequenceId]);
+
+  return (
+    <div>
+      {/* Display the course title above the breadcrumb */}
+      <h2>{course.title}</h2>
+      <nav aria-label="breadcrumb" className="d-inline-block col-sm-10 mb-3">
+        <ol className="list-unstyled d-flex flex-nowrap align-items-center m-0">
+          <li className="list-unstyled col-auto m-0 p-0">
+            <Link
+              className="flex-shrink-0 text-primary"
+              to={`/course/${courseId}/home`}
+              replace
+            >
+              <FontAwesomeIcon icon={faHome} className="mr-2" />
+              <FormattedMessage
+                id="learn.breadcrumb.navigation.course.home"
+                description="The course home link in breadcrumbs nav"
+                defaultMessage="Course"
+              />
+            </Link>
+          </li>
+          {links.map((content, i) => (
+            <CourseBreadcrumb
+              // eslint-disable-next-line react/no-array-index-key
+              key={i}
+              courseId={courseId}
+              sequenceId={sequenceId}
+              content={content}
+              unitId={unitId}
+              withSeparator
+              isStaff={isStaff}
+            />
+          ))}
+        </ol>
+      </nav>
+    </div>
+  );
+};
+
 
 
 CourseBreadcrumbs.propTypes = {
